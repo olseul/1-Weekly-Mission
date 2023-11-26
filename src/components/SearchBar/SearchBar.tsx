bcac25aca7455b59d249acb82a48c30dd6b435c4
olseul <seul9085@naver.com>
2023-11-26 17:00:27 +0900

    feat: x버튼을 누를 시 입력값 초기화

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./SearchBar.css";
 import SearchIcon from "../../assets/icons/SearchIcon";
+import CloseIcon from "../../assets/icons/Close";
 
 interface SearchBarProps {
   setSearchTerm: (searchTerm: string) => void;
@@ -18,9 +19,13 @@ const SearchBar = ({ setSearchTerm }: SearchBarProps) => {
     setSearchTerm(localSearchTerm);
   };
 
+  const handleClear = () => {
+    setLocalSearchTerm("");
+  };
+
   return (
     <div className="search-bar-container">
-      <form onSubmit={handleSubmit}>
+      <form className="search-form" onSubmit={handleSubmit}>
         <SearchIcon />
         <input
           className="search-input"
@@ -29,6 +34,9 @@ const SearchBar = ({ setSearchTerm }: SearchBarProps) => {
           onChange={handleChange}
           placeholder="링크를 검색해 보세요."
         />
+        {localSearchTerm && (
+          <CloseIcon className="close-icon" onClick={handleClear} />
+        )}
       </form>
     </div>
   );
